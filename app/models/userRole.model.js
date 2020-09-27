9c9968d2322bbad8a8044bfacd7f7aad213af687
Cody C <cconroy11@gmail.com>
2020-09-27 02:43:04 -0400

    added user role model

---
+++ app/models/userRole.model.js
@@ -0,0 +1,13 @@
+module.exports = (sequelize, Sequelize) => {
+    const UserRole = sequelize.define("user_roles", {
+      roleId: {
+        type: Sequelize.INTEGER,
+        primaryKey: true
+      },
+      userId: {
+        type: Sequelize.STRING
+      }
+    });
+  
+    return UserRole;
+  };
